14949cfb55a0b72a8cefcc2a71e030c3b0388bc2
David Robinson <dcrobins@systems03.cs.olemiss.edu>
2012-10-10 16:58:00 -0500

    Added turns to TicTacToe

--- src/client/JSONTest.java
+++ src/client/JSONTest.java
@@ -10,10 +10,7 @@ public class JSONTest {
 	 */
 	public static void main(String[] args) throws JSONException {
 		
-		String test = "{\"string\": \"JSON\",\"integer\": \"1\",\"double\": \"2.0\",\"boolean\": \"true\" }";
-		JSONObject a = new JSONObject(test);
-		
-		System.out.println(a.get("string"));
+
 		
 	}
 
